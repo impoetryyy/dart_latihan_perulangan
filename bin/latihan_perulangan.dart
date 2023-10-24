033e99670b29447b2bd720f4095fcf05b61e21db
Putri Meliana <96984201+impoetryyy@users.noreply.github.com>
2023-10-24 17:19:26 +0700

    Add files via upload

---
+++ bin/latihan_perulangan.dart
@@ -0,0 +1,13 @@
+import 'package:latihan_perulangan/latihan_perulangan.dart'
+    as latihan_perulangan;
+
+void main(List<String> arguments) {
+  int angka = 100;
+  var text = '';
+  for (int i = 1; i <= angka; i++) {
+    //print('$i);
+    for (int x = 0; x < 1; x++) {
+      print('$i');
+    }
+  }
+}
